b5502a1cfe0d80939b3f0d3f01c2400aeaa23577
k-oyun <dhdbs001208@naver.com>
2024-02-26 13:43:17 +0900

    Styles part Two

--- Weather/App.js
+++ Weather/App.js
@@ -1,5 +1,9 @@
 import {StatusBar} from "expo-status-bar";
-import {StyleSheet, Text, View} from "react-native";
+import {Dimensions, ScrollView, StyleSheet, Text, View} from "react-native";
+
+//화면의 크기를 얻을 수 있음
+// const {height, width} = Dimensions.get("window");
+const {width: SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
   return (
@@ -9,12 +13,37 @@ export default function App() {
         <Text style={styles.cityName}>서울</Text>
       </View>
 
-      <View style={styles.weather}>
+      <ScrollView
+        //page화
+        pagingEnabled
+        //스크롤을 수평으로
+        horizontal
+        //스크롤 상태바 스타일 수정
+        //IOS에서만 가능
+        // indicatorStyle="white"
+
+        //스크롤 상태바를 숨김
+        showsHorizontalScrollIndicator={false}
+        //스크롤 뷰는 contentContainerStyle를 사용해야함
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -25,8 +54,7 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
   },
   city: {
-    flex: 1,
-
+    flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -35,12 +63,10 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: 500,
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
 
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
