9350ed8a14ec1137bb92096dd559c75d9f7e27e2
k-oyun <dhdbs001208@naver.com>
2024-02-26 12:33:54 +0900

    Update App.js
    ## Website가 아니다!
    
    - div는 사용 불가능→ view를 사용해야함!!
        - 항상 view를 import해야함
    - span → text | 모든 텍스트는 Text를 통해 사용해야한다
        - text는 view가 아닌 text component로 들어가야함!
        - 기존 방식 처럼 div에 들어갈 수 없음
    - style은 react js와 유사함 but 몇몇은 사용할 수 없음! ex) border
        - so stylesheet사용
            - 자동완성 사용 가능!
    - status-bar
        - 핸드폰 화면 상단의 시계, 배터리, wifi를 의미
        - option
            - auto
            - light
            - dark
    - React Js와 달리 CSS에러를 알려준다!

--- Weather/App.js
+++ Weather/App.js
@@ -4,8 +4,8 @@ import {StyleSheet, Text, View} from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello! I made a RN App!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
   },
 });
