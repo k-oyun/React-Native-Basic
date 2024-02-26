4d6d86821ab022274d73aa9740e43a9c27bee6ab
k-oyun <dhdbs001208@naver.com>
2024-02-26 13:25:12 +0900

    Styles

--- Weather/App.js
+++ Weather/App.js
@@ -3,10 +3,52 @@ import {StyleSheet, Text, View} from "react-native";
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: "tomato"}}></View>
-      <View style={{flex: 3, backgroundColor: "teal"}}></View>
-      <View style={{flex: 1, backgroundColor: "orange"}}></View>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  city: {
+    flex: 1,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  cityName: {
+    fontSize: 68,
+    fontWeight: 500,
+  },
+  weather: {
+    flex: 3,
+  },
+
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+});
